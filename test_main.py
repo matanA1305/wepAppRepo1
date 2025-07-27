b0e0edf7ee6958b1ad2da35841faef89c1f48576
מתן אזרזר <you@example.com>
2025-07-27 20:31:23 +0300

    save

--- test_main.py
+++ test_main.py
@@ -2,8 +2,6 @@ import pytest
 from httpx import AsyncClient
 from main import app
 
-import asyncio
-
 @pytest.mark.asyncio
 async def test_read_root():
     async with AsyncClient(app=app, base_url="http://test") as ac:
